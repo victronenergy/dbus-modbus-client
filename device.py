fee7036365bbfb7e4c4c5c5e27abcde8dd2b71d7
Mans Rullgard <mans@mansr.com>
2020-06-19 12:08:15 +0100

    device: improve response type checking

--- device.py
+++ device.py
@@ -3,7 +3,7 @@ import dbus
 from functools import partial
 from pymodbus.client.sync import *
 from pymodbus.utilities import computeCRC
-from pymodbus.pdu import ExceptionResponse
+from pymodbus.register_read_message import ReadHoldingRegistersResponse
 import logging
 import os
 import struct
@@ -107,7 +107,7 @@ class ModbusDevice(object):
             rr = self.modbus.read_holding_registers(start, count,
                                                     unit=self.unit)
 
-        if isinstance(rr, ExceptionResponse):
+        if not isinstance(rr, ReadHoldingRegistersResponse):
             log.error('Error reading registers %#04x-%#04x: %s',
                       start, start + count - 1, rr)
             raise Exception(rr)
