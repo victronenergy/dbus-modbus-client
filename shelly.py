a8a802824196a71bf0ffa26b275b9107c0aa4465
Sam Stenvall <sam.stenvall@nitor.com>
2023-10-17 09:47:14 +0300

    shelly: implement energy readings too
    These are exposed by a separate "EMData" component so I initially missed them

--- shelly.py
+++ shelly.py
@@ -27,13 +27,21 @@ class ShellyEnergyMeter(device.EnergyMeter):
         ]
 
         self.data_regs = [
+            Reg_f32l(1162, '/Ac/Energy/Forward', 1000, '%.1f kWh'),
+            Reg_f32l(1164, '/Ac/Energy/Reverse', 1000, '%.1f kWh'),
             Reg_f32l(1013, '/Ac/Power', 1, '%.1f W'),
+            Reg_f32l(1182, '/Ac/L1/Energy/Forward', 1000, '%.1f kWh'),
+            Reg_f32l(1184, '/Ac/L1/Energy/Reverse', 1000, '%.1f kWh'),
             Reg_f32l(1020, '/Ac/L1/Voltage', 1, '%.1f V'),
             Reg_f32l(1022, '/Ac/L1/Current', 1, '%.1f A'),
             Reg_f32l(1024, '/Ac/L1/Power', 1, '%.1f W'),
+            Reg_f32l(1202, '/Ac/L2/Energy/Forward', 1000, '%.1f kWh'),
+            Reg_f32l(1204, '/Ac/L2/Energy/Reverse', 1000, '%.1f kWh'),
             Reg_f32l(1040, '/Ac/L2/Voltage', 1, '%.1f V'),
             Reg_f32l(1042, '/Ac/L2/Current', 1, '%.1f A'),
             Reg_f32l(1044, '/Ac/L2/Power', 1, '%.1f W'),
+            Reg_f32l(1222, '/Ac/L3/Energy/Forward', 1000, '%.1f kWh'),
+            Reg_f32l(1224, '/Ac/L3/Energy/Reverse', 1000, '%.1f kWh'),
             Reg_f32l(1060, '/Ac/L3/Voltage', 1, '%.1f V'),
             Reg_f32l(1062, '/Ac/L3/Current', 1, '%.1f A'),
             Reg_f32l(1064, '/Ac/L3/Power', 1, '%.1f W'),
